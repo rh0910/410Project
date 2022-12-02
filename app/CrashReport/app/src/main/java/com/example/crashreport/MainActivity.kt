8bb891f20598fb891306a62eaf7e5d3e5e09c28b
fh69 <choudhury.tamzid@gmail.com>
2022-12-02 13:56:00 -0500

    playing around android studio

--- app/CrashReport/app/src/main/java/com/example/crashreport/MainActivity.kt
+++ app/CrashReport/app/src/main/java/com/example/crashreport/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.crashreport
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.crashreport.ui.theme.CrashReportTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,13 +32,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Green) {
+        Text(text = "We are $name!", modifier = Modifier.padding(15.dp))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CrashReportTheme {
-        Greeting("Android")
+        Greeting("proj6 collision detection")
     }
 }
